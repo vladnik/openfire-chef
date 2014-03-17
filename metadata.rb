11c4f035820bcb9a10f4376b941a5ef902bbbf76
vladnik <vladnik@demandbase.com>
2014-03-17 00:51:59 -0700

    Updated version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "gavin@leftbrained.co.uk"
 license          "Apache 2.0"
 description      "Installs Openfire Jabber server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 supports         "ubuntu"
 supports         "centos"
 supports         "redhat"
